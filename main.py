65fe833a40a7eda01b23ef8153b54b84ac1898b2
Classy-Brian <phanbrian101@gmail.com>
2025-10-17 16:43:34 -0700

    Added get_word_count()

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         print(file_contents)
 
+def get_word_count(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        word_count = len(file_contents.split())
+        print(f" Found {word_count} total words")
+
 def main():
     path_to_file = "books/frankenstein.txt"
-    get_book_text(path_to_file)
+    # get_book_text(path_to_file)
+    get_word_count(path_to_file)
 
 main()
