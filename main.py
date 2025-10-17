c21d2a7312eae5304629e9c3376b8e6bade364f6
Classy-Brian <phanbrian101@gmail.com>
2025-10-17 16:58:01 -0700

    Added get_count_characters() in stats.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from stats import get_word_count
+from stats import get_word_count, get_count_characters
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
 def main():
     path_to_file = "books/frankenstein.txt"
-    # get_book_text(path_to_file)
-    get_word_count(path_to_file)
+    text = get_book_text(path_to_file)
+    # get_word_count(path_to_file)
+    char_count = get_count_characters(text)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,4 +2,16 @@ def get_word_count(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         word_count = len(file_contents.split())
-        print(f" Found {word_count} total words")
+        return word_count
+
+def get_count_characters(text):
+    words = text.lower().split()
+    my_dict = {}
+    for word in words:
+        for char in word:
+            if char in my_dict:
+                my_dict[char] += 1
+            else:
+                my_dict[char] = 1
+    return my_dict
+
