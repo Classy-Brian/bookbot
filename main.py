a729a46b5cab1b9c70b1dd18486bd825807431cf
Classy-Brian <phanbrian101@gmail.com>
2025-10-17 16:38:52 -0700

    Reads file

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    get_book_text(path_to_file)
+
+main()
