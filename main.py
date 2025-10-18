e9b24ba15276cbe706aafd1007847e054facce50
Classy-Brian <phanbrian101@gmail.com>
2025-10-17 18:00:36 -0700

    Print report: build sorted char list, filter alpha, add headers and word count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_count_characters
+from stats import get_word_count, get_count_characters, print_report
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,9 +7,12 @@ def get_book_text(path_to_file):
 
 def main():
     path_to_file = "books/frankenstein.txt"
+    
     text = get_book_text(path_to_file)
-    # get_word_count(path_to_file)
+    word_count = get_word_count(path_to_file)
     char_count = get_count_characters(text)
-    print(char_count)
+
+    print_report(char_count, word_count, path_to_file)
+
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,28 @@ def get_count_characters(text):
                 my_dict[char] = 1
     return my_dict
 
+def sort_on(items):
+    return items["num"]
+
+def build_sorted_list(char_count):
+    items = []
+    for char, count in char_count.items():
+        items.append({'char': char, 'num': count})
+    items.sort(key=sort_on, reverse=True)
+    return items
+
+def print_report(char_count, word_count, path_to_file):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+
+    print("--------- Character Count -------")
+    sorted_list = build_sorted_list(char_count)
+    for elem in sorted_list:
+        if elem['char'].isalpha():
+            print(f"{elem['char']}: {elem['num']}")
+    
+    print("============= END ===============")
+    
