3cc76b43a0b06502b6ae0306b2899a098eb5fca7
Classy-Brian <phanbrian101@gmail.com>
2025-10-17 21:01:56 -0700

    Added sys arguments where user sends in a path to the book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count, get_count_characters, print_report
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,13 +7,18 @@ def get_book_text(path_to_file):
         return file_contents
 
 def main():
-    path_to_file = "books/frankenstein.txt"
-    
-    text = get_book_text(path_to_file)
-    word_count = get_word_count(path_to_file)
-    char_count = get_count_characters(text)
 
-    print_report(char_count, word_count, path_to_file)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_file = sys.argv[1]
+        
+        text = get_book_text(path_to_file)
+        word_count = get_word_count(path_to_file)
+        char_count = get_count_characters(text)
+
+        print_report(char_count, word_count, path_to_file)
 
 
 main()
